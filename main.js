8bbcedd3f6dddb0469bd42dc178a2628e8ec0d06
Vinit Hemadri <vineet.vh@gmail.com>
2022-01-06 00:17:02 +0530

    Updated todos to localstorage

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const todoList = document.querySelector(".todo-items");
 
 let todos = [];
 
-todoInput.addEventListener("change", saveTodo);
+todoInput.addEventListener("change", handleTodo);
 
-function saveTodo(e) {
+function handleTodo(e) {
   let todo = e.target.value;
   if (todo.length > 200) {
     console.log("Lengthy");
@@ -13,21 +13,28 @@ function saveTodo(e) {
   let todoObj = {
     title: todo,
   };
-  todos.push(todoObj);
-  console.log(todos);
-  e.target.value = "";
-  displayTodo(todos);
+  todos = [...todos, todoObj];
+  saveTodos();
+  displayTodo();
 }
 
-function displayTodo(todos) {
-  todos.forEach((todo) => {
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function displayTodo() {
+  let todoItems = JSON.parse(localStorage.getItem("todos"));
+  console.log(todoItems);
+  todoItems.forEach((todo) => {
     todoList.innerHTML += `
-    <li class="todo-list">
-    <span class="list-check"></span>
-    ${todo.title}
-    <span class="cancel-btn">x</span>
-    </li>
-    <span class="line"></span>
-    `;
+      <li class="todo-list">
+      <span class="list-check"></span>
+      ${todo.title}
+      <span class="cancel-btn">x</span>
+      </li>
+      <span class="line"></span>
+      `;
   });
 }
+
+displayTodo();
